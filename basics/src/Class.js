25c49f043b198d4ffa823ce81a754efc3e0c830b
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-10-19 22:56:08 +0530

    class component

--- basics/src/Class.js
+++ basics/src/Class.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 
 export default class Userclass extends Component {
-   
+   render(){
+    return(
+        <h1>Heelloo from Class Component</h1>
+    )
+   }
 }
